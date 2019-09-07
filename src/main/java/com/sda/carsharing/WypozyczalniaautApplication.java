f5fb818fb9f40aacb6d96d89f94a5a0d6a351725
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2019-09-07 12:21:53 +0200

    Przygotowanie projektu

---
+++ src/main/java/com/sda/carsharing/WypozyczalniaautApplication.java
@@ -0,0 +1,13 @@
+package com.sda.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypozyczalniaautApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WypozyczalniaautApplication.class, args);
+    }
+
+}
