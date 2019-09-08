631433801344715672a4630f61905eb0d7a34305
mrMouse <ia@asygnata.com>
2019-09-08 12:02:31 +0200

    DTO notempty to notblank

--- src/main/java/com/sda/carsharing/dto/CarModelDto.java
+++ src/main/java/com/sda/carsharing/dto/CarModelDto.java
@@ -3,39 +3,42 @@ package com.sda.carsharing.dto;
 import com.sda.carsharing.model.enums.*;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class CarModelDto {
 
-    @NotEmpty
+    @NotBlank
+    private Long id;
+
+    @NotBlank
     private CarClass carClass;
 
-    @NotEmpty
+    @NotBlank
     private String mark;
 
-    @NotEmpty
+    @NotBlank
     private String model;
 
-    @NotEmpty
+    @NotBlank
     private BodyType bodyType;
 
-    @NotEmpty
+    @NotBlank
     private int productionDate;
 
-    @NotEmpty
+    @NotBlank
     private Color color;
 
-    @NotEmpty
+    @NotBlank
     private Gear gear;
 
-    @NotEmpty
+    @NotBlank
     private Fuel fuel;
 
-    @NotEmpty
+    @NotBlank
     private boolean airCondition;
 
-    @NotEmpty
+    @NotBlank
     private BigDecimal price;
 }
--- src/main/java/com/sda/carsharing/web/controllers/CarModelController.java
+++ src/main/java/com/sda/carsharing/web/controllers/CarModelController.java
@@ -1,14 +1,23 @@
 package com.sda.carsharing.web.controllers;
 
 import com.sda.carsharing.services.CarModelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CarModelController {
     private CarModelService carModelService;
 
-//    @Autowired
-//    public CarModelController(CarModelService carModelService) {
-//        this.carModelService = carModelService;
-//    }
+    @Autowired
+    public CarModelController(CarModelService carModelService) {
+        this.carModelService = carModelService;
+    }
+
+    @RequestMapping(value = "/carmodel", method = RequestMethod.GET)
+    public String showAll(){
+
+        return "show"
+    }
 }
