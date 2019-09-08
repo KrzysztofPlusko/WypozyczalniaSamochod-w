5ce214c278cdd2734cc34b3b2cf95f69f8de8c0d
mrMouse <ia@asygnata.com>
2019-09-08 11:49:00 +0200

    carmodel controller

--- src/main/java/com/sda/carsharing/web/controllers/CarModelController.java
+++ src/main/java/com/sda/carsharing/web/controllers/CarModelController.java
@@ -1,4 +1,14 @@
 package com.sda.carsharing.web.controllers;
 
+import com.sda.carsharing.services.CarModelService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CarModelController {
+    private CarModelService carModelService;
+
+//    @Autowired
+//    public CarModelController(CarModelService carModelService) {
+//        this.carModelService = carModelService;
+//    }
 }
