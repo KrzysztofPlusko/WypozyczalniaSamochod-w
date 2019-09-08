1d167a8d36c5306e02ae9dc87a4ac5dfefa46a80
Maciek <pagganini@wp.pl>
2019-09-08 10:12:24 +0200

    klasa employee dopisałem @Column

--- src/main/java/com/sda/carsharing/model/entities/Employee.java
+++ src/main/java/com/sda/carsharing/model/entities/Employee.java
@@ -5,6 +5,7 @@ package com.sda.carsharing.model.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -16,7 +17,9 @@ import java.util.Objects;
 public class Employee {
     @Id
     private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     @Override
