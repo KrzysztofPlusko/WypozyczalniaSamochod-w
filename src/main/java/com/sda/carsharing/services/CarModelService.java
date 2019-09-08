c29cd2105126c47ee8729a611b22e85af60502b7
mrMouse <ia@asygnata.com>
2019-09-08 11:40:04 +0200

    Carmodel DTO, Mapper, Service

---
+++ src/main/java/com/sda/carsharing/services/CarModelService.java
@@ -0,0 +1,30 @@
+package com.sda.carsharing.services;
+
+import com.sda.carsharing.dto.CarModelDto;
+import com.sda.carsharing.mappers.CarModelMapper;
+import com.sda.carsharing.model.entities.CarModel;
+import com.sda.carsharing.model.repositories.CarModelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+public class CarModelService {
+    private CarModelRepository carModelRepository;
+
+    @Autowired
+    public CarModelService(CarModelRepository carModelRepository) {
+        this.carModelRepository = carModelRepository;
+    }
+
+    @Transactional
+    public List<CarModel> findAll(){
+        return carModelRepository.findAll();
+    }
+
+    @Transactional
+    public void addCarModel(CarModelDto carModelDto){
+        carModelRepository.save(CarModelMapper.toEntity(carModelDto));
+    }
+}
---
+++ src/main/java/com/sda/carsharing/web/controllers/CarModelController.java
@@ -0,0 +1,4 @@
+package com.sda.carsharing.web.controllers;
+
+public class CarModelController {
+}
