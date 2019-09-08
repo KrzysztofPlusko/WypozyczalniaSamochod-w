fef40bb1e17a8167bf0e436605c5093b65eab46d
mrMouse <ia@asygnata.com>
2019-09-08 09:55:33 +0200

    carmodel literowka

--- src/main/java/com/sda/carsharing/model/entities/CarModel.java
+++ src/main/java/com/sda/carsharing/model/entities/CarModel.java
@@ -1,16 +1,12 @@
 package com.sda.carsharing.model.entities;
 
 import com.sda.carsharing.model.enums.*;
-import com.sun.corba.se.impl.encoding.CodeSetConversion;
-
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Entity
 @Table(name = "car_models")
@@ -24,7 +20,7 @@ public class CarModel extends BaseEntity{
     private int productionDate;
     private Color color;
     private Gear gear;
-    private Fuel fule;
+    private Fuel fuel;
     private boolean airCondition;
     private BigDecimal price;
 
@@ -38,7 +34,7 @@ public class CarModel extends BaseEntity{
                 ", productionDate=" + productionDate +
                 ", color=" + color +
                 ", gear=" + gear +
-                ", fule=" + fule +
+                ", fuel=" + fuel +
                 ", airCondition=" + airCondition +
                 ", price=" + price +
                 "} " + super.toString();
