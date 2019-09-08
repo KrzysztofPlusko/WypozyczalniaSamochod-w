74c60730cf998edd203744ed92edbc980d66db9f
mrMouse <ia@asygnata.com>
2019-09-08 10:00:46 +0200

    carmodel adnotacje

--- src/main/java/com/sda/carsharing/model/entities/CarModel.java
+++ src/main/java/com/sda/carsharing/model/entities/CarModel.java
@@ -4,24 +4,39 @@ import com.sda.carsharing.model.enums.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "car_models")
-@Getter @Setter
-public class CarModel extends BaseEntity{
+@Getter
+@Setter
+public class CarModel extends BaseEntity {
 
+    @Column(name = "car_class")
     private CarClass carClass;
+
     private String mark;
+
     private String model;
+
+    @Column(name = "body_type")
     private BodyType bodyType;
+
+    @Column(name="production_date")
     private int productionDate;
+
     private Color color;
+
     private Gear gear;
+
     private Fuel fuel;
+
+    @Column(name="air_condition")
     private boolean airCondition;
+
     private BigDecimal price;
 
     @Override
