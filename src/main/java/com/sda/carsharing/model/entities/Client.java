5e978568d96281850f575a8b361db8225479973e
Your Name <you@example.com>
2019-09-08 09:39:09 +0200

    Struktura Projektu Entity Client

--- src/main/java/com/sda/carsharing/model/entities/Client.java
+++ src/main/java/com/sda/carsharing/model/entities/Client.java
@@ -1,4 +1,35 @@
 package com.sda.carsharing.model.entities;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+
+    @NotBlank
+    @Column(name = "first_name", updatable = false, nullable = false)
+    private String first_name;
+
+    @NotBlank
+    @Column(name = "last_name", updatable = false, nullable = false)
+    private String last_name;
+    @NotBlank
+    @Column(name = "email", updatable = false, nullable = false)
+    private String email;
+    @NotBlank
+    @Column(name = "adres", updatable = false, nullable = false)
+    private String adres;
+
 }
