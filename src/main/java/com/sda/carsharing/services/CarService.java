166525aafc8cfc34ca535bc0f66394bbb45fea9d
mrMouse88 <ia@asygnata.com>
2019-09-10 00:05:01 +0200

    service save

--- src/main/java/com/sda/carsharing/services/CarService.java
+++ src/main/java/com/sda/carsharing/services/CarService.java
@@ -1,5 +1,7 @@
 package com.sda.carsharing.services;
 
+import com.sda.carsharing.dto.CarDto;
+import com.sda.carsharing.mappers.CarMapper;
 import com.sda.carsharing.model.entities.Car;
 import com.sda.carsharing.model.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +22,10 @@ public class CarService {
     @Transactional
     public List<Car> findAll(){
         return carRepository.findAll();
+    }
+
+    @Transactional
+    public void saveCar(CarDto carDto){
+        carRepository.save(CarMapper.toEntity(carDto));
     }
 }
