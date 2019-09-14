7c784f3f0e2a80301f941c2b79569011a6d30fcd
Your Name <you@example.com>
2019-09-14 12:35:22 +0200

    Dopisanie sidebar parametrów Employee...

--- src/main/java/com/sda/carsharing/web/controllers/EmployeeController.java
+++ src/main/java/com/sda/carsharing/web/controllers/EmployeeController.java
@@ -18,126 +18,45 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
 
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
+    //wyświetl listę wszyskich pracowników
+    @GetMapping
+    public String showAllView(Model model) {
+        model.addAttribute("employees", employeeService.findAll());
+        return "admin/employee-list";
+    }
 
+    //widok dla dodania nowego pracownika do bazy
+    @GetMapping(value = "add")
+    public String addEmployeeViev(Model model) {
+        model.addAttribute("employeeDto", new EmployeeDto());
+        return "admin/employee-form";
+    }
 
-        @Autowired
-        public EmployeeController(EmployeeService employeeService) {
-            this.employeeService = employeeService;
-        }
+    //logika dodania nowego modelu auta do bazy
+    @PostMapping
+    public String addEmployee(@Valid @ModelAttribute("employeeDto") EmployeeDto employeeDto, Model model) {
 
-        //wyświetl listę wszyskich pracowników
-        @GetMapping
-        public String showAllView(Model model) {
-            model.addAttribute("employees", employeeService.findAll());
-            return "admin/employee-list";
-        }
+        this.employeeService.addEmployee(employeeDto);
 
-        //widok dla dodania nowego pracownika do bazy
-        @GetMapping(value = "add")
-        public String addEmployeeViev(Model model){
-            model.addAttribute("employeeDto", new EmployeeDto());
-            return "admin/employee-form";
-        }
+        model.addAttribute("msg", "Pracownik dodany");
+        model.addAttribute("employee", employeeService.findAll());
+        return "admin/showemployeelist";
+    }
 
-        //logika dodania nowego modelu auta do bazy
-        @PostMapping
-        public String addEmployee(@Valid @ModelAttribute("employeeDto") EmployeeDto employeeDto, Model model) {
-
-            this.employeeService.addEmployee(employeeDto);
-
-            model.addAttribute("msg", "Pracownik dodany");
-            model.addAttribute("employee", employeeService.findAll());
-            return "admin/showemployeelist";
-        }
-
-        @PostMapping(value = "delp")
-        public String delpEmployee(@RequestParam(required = true) Long id, Model model){
-            this.employeeService.deleteEmployeeById(id);
-            model.addAttribute("msg", "Pracownik usunięty");
-            model.addAttribute("employees", employeeService.findAll());
-            return "admin/showemployeelist";
-        }
-
-
-
-
-
-
-
-//    @RequestMapping(value = "/addEmployee", method = RequestMethod.GET)
-//    public String addEmployeeViev(Model model) {
-//        model.addAttribute("employeeDto", new EmployeeDto());
-//        return "add_employee";
-//    }
-//
-//    @PostMapping(value = "/employee")
-//    public String createEmployee(@Valid @ModelAttribute("employeeDto") EmployeeDto employeeDto, Model model) {
-//        if (employeeDto.getId() == null) {
-//            this.employeeService.createEmployee(employeeDto);
-//
-//        } else {
-//            this.employeeService.updateEmployee(employeeDto);
-//        }
-//        model.addAttribute("employees", this.employeeService.findAll());
-//        return "employess";
-//    }
-//
-//    @PostMapping(value = "/deleteEmployee")
-//   public String deleteEmployee(@RequestParam(required = true) Long id, Model model){
-//        this.employeeService.deleteEmployeeById(id);
-//        model.addAttribute("employees", this.employeeService.findAll());
-//
-//        return "employess";
-//   }
-//   @PostMapping(value = "/editEmployess")
-//    public String editEmployee(@RequestParam(required = true) Long id, Model model){
-//        model.addAttribute("employeeDto", toDto (this.employeeService.getById(id)));
-//
-//        return "add_employee";
-
-
+    @PostMapping(value = "delp")
+    public String delpEmployee(@RequestParam(required = true) Long id, Model model) {
+        this.employeeService.deleteEmployeeById(id);
+        model.addAttribute("msg", "Pracownik usunięty");
+        model.addAttribute("employees", employeeService.findAll());
+        return "admin/showemployeelist";
+    }
 
 
 }
 
-//    private UserService userService;
-////
-////    @Autowired
-////    public UserController(UserService userService) {
-////        this.userService = userService;
-////    }
-////
-////    @RequestMapping(value = "/adduser", method = RequestMethod.GET)
-////    public String addUserView(Model model){
-////        model.addAttribute("userDto", new UserDto());
-////        return "add_user";
-////    }
-////
-////    @PostMapping(value = "/user")
-////    public String createUser(@Valid @ModelAttribute("userDto") UserDto userDto, Model model){
-////
-////        if(userDto.getId() == null) {
-////            this.userService.createUser(userDto);
-////        } else {
-////            this.userService.updateUser(userDto);
-////        }
-////
-////        model.addAttribute("users", this.userService.findAll());
-////        return "users";
-////    }
-////
-////    @PostMapping(value = "/deleteUser")
-////    public String deleteUser(@RequestParam(required = true) Long id, Model model){
-////        this.userService.deleteUserById(id);
-////        model.addAttribute("users", this.userService.findAll());
-////
-////        return "users";
-////    }
-////
-////    @PostMapping(value = "/editUser")
-////    public String editUser(@RequestParam(required = true) Long id, Model model){
-////        model.addAttribute("userDto", toDto(this.userService.getById(id)));
-////
-////        return "add_user";
-////    }
+
