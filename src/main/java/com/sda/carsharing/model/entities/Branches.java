a2f43e3cdb0f9317b94417cdd6bd829ead6ccdad
Maciek <pagganini@wp.pl>
2019-09-14 14:27:24 +0200

    branch oddziały

--- src/main/java/com/sda/carsharing/model/entities/Branches.java
+++ src/main/java/com/sda/carsharing/model/entities/Branches.java
@@ -1,4 +1,26 @@
 package com.sda.carsharing.model.entities;
 
-public class Branches {
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Branches")
+
+public class Branches extends BaseEntity{
+
+    @Column(name = "cars")
+    @NotBlank
+    private Car car;
+
+    @Column(name = "adress")
+    @NotBlank
+    private String Adress;
+
+    @Column(name = "employees")
+    @NotBlank
+    private Employee employee;
+
 }
