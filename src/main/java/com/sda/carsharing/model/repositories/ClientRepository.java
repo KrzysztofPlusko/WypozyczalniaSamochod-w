25ed45afc9dc02c9ad32cd310666718a893e07a9
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2019-09-15 09:11:06 +0200

    ClientService uzupełnienie metod

--- src/main/java/com/sda/carsharing/model/repositories/ClientRepository.java
+++ src/main/java/com/sda/carsharing/model/repositories/ClientRepository.java
@@ -2,10 +2,11 @@ package com.sda.carsharing.model.repositories;
 
 import com.sda.carsharing.model.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
-
+    @Query("SELECT c FROM Client c WHERE c.email = ?1")
     List<Client> FindAllByUsername(String email);
 }
