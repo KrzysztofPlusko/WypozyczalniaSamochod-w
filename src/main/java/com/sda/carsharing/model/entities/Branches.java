e7578deb80fea824e3203e8a096ffb4aee9d2b68
Maciek <pagganini@wp.pl>
2019-09-15 10:56:47 +0200

    poprawiony oddzial :)

--- src/main/java/com/sda/carsharing/model/entities/Branches.java
+++ src/main/java/com/sda/carsharing/model/entities/Branches.java
@@ -1,4 +1,30 @@
 package com.sda.carsharing.model.entities;
 
-public class Branches {
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "Branches")
+@Setter @Getter
+public class Branches extends BaseEntity{
+    private String name;
+    @Column(name = "cars")
+    @NotBlank
+    private List<Car> cars;
+
+    @Column(name = "adress")
+    @NotBlank
+    private String Adress;
+
+    @Column(name = "employees")
+    @NotBlank
+    private List<Employee> employees;
+
 }
