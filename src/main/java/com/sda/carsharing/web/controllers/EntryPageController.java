64f7fe78abcee2cb202d1fe87ab1e6cd4dbed41a
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2019-09-21 13:55:35 +0200

    usunięcie kontrolerów logowaniea

--- src/main/java/com/sda/carsharing/web/controllers/EntryPageController.java
+++ src/main/java/com/sda/carsharing/web/controllers/EntryPageController.java
@@ -4,6 +4,7 @@ package com.sda.carsharing.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+/*
 
 @Controller
 @RequestMapping("/")
@@ -14,3 +15,4 @@ public class EntryPageController {
         return "login-page";
     }
 }
+*/
